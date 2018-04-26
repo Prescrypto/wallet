7fcf4aad1b15d04bda9c1c1c490bfb6d8ebc7acc
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-04-25 20:05:16 -0500

    add alert

--- uikit/js/wallet.js
+++ uikit/js/wallet.js
@@ -1,8 +1,21 @@
 var PUBLIC_KEY;
 var PRIVATE_KEY;
+
+// alert templating
+
+var alert_success_template = '\
+    <div class="alert alert-success alert-dismissible fade show" role="alert"> \
+      <strong>Success Key Saved!</strong> You can go to the next page safety! \
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"> \
+        <span aria-hidden="true">&times;</span> \
+      </button> \
+    </div>'
+
+
 function save_key(){
   localforage.setItem('privatekey', $("#privkey").val());
   localforage.setItem('publickey', $("#pubkey").val());
+  $("#result").html(alert_success_template);
 }
 function show_publickey()
 {
