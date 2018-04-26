b155f08d4fd106cdac9648cf8baa0aa96d3ed467
MEvangelista <miguel.eva.alv@gmail.com>
2018-04-26 17:23:27 -0500

    add function validate key, validate pubkey and validate privkey

--- uikit/js/wallet.js
+++ uikit/js/wallet.js
@@ -1,5 +1,7 @@
 var PUBLIC_KEY;
 var PRIVATE_KEY;
+var PRIVKEY_VALUE;
+var PUBKEY_VALUE;
 
 // alert templating
 
@@ -11,27 +13,57 @@ var alert_success_template = '\
       </button> \
     </div>'
 
+var alert_error_template = '\
+    <div class="alert alert-danger alert-dismissible fade show" role="alert"> \
+      <strong>ERROR!</strong> Please, please check your keys! \
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"> \
+        <span aria-hidden="true">&times;</span> \
+      </button> \
+    </div>'
+
 
 function save_key(){
   localforage.setItem('privatekey', $("#privkey").val());
   localforage.setItem('publickey', $("#pubkey").val());
-  $("#result").html(alert_success_template);
+//$("#result").html(alert_success_template);
 }
-function show_publickey()
-{
+
+function validate_pubkey(){
+  if ($("#pubkey").val().length===0){
+    return false
+  }else{
+    return true
+  }
+}
+function validate_privkey(){
+  if($("#privkey").val().lengthh===0){
+      return false
+  }else{
+      return true
+  }
+}
+function validate_keys(){
+  save_key()
+  if (validate_privkey() && validate_pubkey()){
+    $("#result").html(alert_success_template);
+  }else{
+    $("#result").html(alert_error_template);
+  }
+}
+function show_publickey(){
+  var value;
 localforage.getItem('publickey', function(err, value){
   // Run this code once the value has been
   // loaded from the offline store.
   PUBLIC_KEY = value;
   //$('#result').html(value);
-});
+  });
 }
-function show_privatekey()
-{
-var _value;
+function show_privatekey(){
+  var _value;
 localforage.getItem('privatekey', function(err, value){
   // Run this code once the value has been
   // loaded from the offline store.
   PRIVATE_KEY = value;
-});
+  });
 }
