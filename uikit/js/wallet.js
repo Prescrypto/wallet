e5ab39b206baf628d9087e7ab72c682e39424a5d
Miguel Evangelista <miguel.eva.alv@gmail.com>
2018-06-11 13:20:47 -0500

    add library jsrsasign

--- uikit/js/wallet.js
+++ uikit/js/wallet.js
@@ -121,56 +121,56 @@ $('#next_button').click(function (){
 
 });
 
-function decrypt_fields(){
-  // descrypt function
-  var decrypt = new JSEncrypt();
-  decrypt.setPrivateKey($('#priv_key').val());
+// function decrypt_fields(){
+//   // descrypt function
+//   var decrypt = new JSEncrypt();
+//   decrypt.setPrivateKey($('#priv_key').val());
 
-  var location_text = decrypt.decrypt($('#location').html());
-  $("#location").html(location_text);
-  console.log("Resultado "+ location_text);
+//   var location_text = decrypt.decrypt($('#location').html());
+//   $("#location").html(location_text);
+//   console.log("Resultado "+ location_text);
 
-  var sender_text = decrypt.decrypt($('#sender').html());
-  $("#sender").html(sender_text);
-  console.log("Resultado "+ sender_text);
+//   var sender_text = decrypt.decrypt($('#sender').html());
+//   $("#sender").html(sender_text);
+//   console.log("Resultado "+ sender_text);
 
-  var receiver_text = decrypt.decrypt($('#receiver').html());
-  $("#receiver").html(receiver_text);
-  console.log("Resultado "+ receiver_text);
-}
+//   var receiver_text = decrypt.decrypt($('#receiver').html());
+//   $("#receiver").html(receiver_text);
+//   console.log("Resultado "+ receiver_text);
+// }
 
-$('#button_decrypt').click(function () {
-  decrypt_fields();
-  $("#button_decrypt").attr("disabled", true);
-  $("#button_encrypt").removeAttr("disabled");
-});
+// $('#button_decrypt').click(function () {
+//   decrypt_fields();
+//   $("#button_decrypt").attr("disabled", true);
+//   $("#button_encrypt").removeAttr("disabled");
+// });
 
-function encrypt_fields(){
-// Encrypt function
-  var encrypt_object = new JSEncrypt();
-  encrypt_object.setPublicKey($('#pub_key').val());
+// function encrypt_fields(){
+// // Encrypt function
+//   var encrypt_object = new JSEncrypt();
+//   encrypt_object.setPublicKey($('#pub_key').val());
 
-  var location_text = $("#location").html().trim();
-  location_text = encrypt_object.encrypt(location_text);
-  $("#location").html(location_text);
-  console.log("Resultado "+ location_text);
+//   var location_text = $("#location").html().trim();
+//   location_text = encrypt_object.encrypt(location_text);
+//   $("#location").html(location_text);
+//   console.log("Resultado "+ location_text);
 
-  var sender_text = $("#sender").html().trim();
-  sender_text = encrypt_object.encrypt(sender_text);
-  $("#sender").html(sender_text);
-  console.log("Resultado "+ sender_text);
+//   var sender_text = $("#sender").html().trim();
+//   sender_text = encrypt_object.encrypt(sender_text);
+//   $("#sender").html(sender_text);
+//   console.log("Resultado "+ sender_text);
 
-  var receiver_text = $("#receiver").html().trim();
-  receiver_text = encrypt_object.encrypt(receiver_text);
-  $("#receiver").html(receiver_text);
-  console.log("Resultado "+ receiver_text);
-}
+//   var receiver_text = $("#receiver").html().trim();
+//   receiver_text = encrypt_object.encrypt(receiver_text);
+//   $("#receiver").html(receiver_text);
+//   console.log("Resultado "+ receiver_text);
+// }
 
-$('#button_encrypt').click(function(){
-  encrypt_fields();
-  $("#button_decrypt").removeAttr("disabled");
-  $("#button_encrypt").attr("disabled", true);
-});
+// $('#button_encrypt').click(function(){
+//   encrypt_fields();
+//   $("#button_decrypt").removeAttr("disabled");
+//   $("#button_encrypt").attr("disabled", true);
+// });
 
 $("#button_make_tx").click(function(){
   //$("#forms").html(forms);
@@ -221,8 +221,111 @@ function encrypt_tx(){
   });
 }
 
-$('#button_send_tx').click(function(){
-  encrypt_tx();
+function encrypt_data(data, public_key){
+
+  var encrypt_object = new JSEncrypt();
+  //We assign public key to the constructor
+  encrypt_object.setPublicKey(public_key);
+  //return data encrypt
+  return encrypt_object.encrypt(data);
+}
+
+function sign_msg(msg){
+
+  var rsa = new RSAKey();
+  //Load private key to variable rsa
+  rsa.readPrivateKeyFromPEMString($('#priv_key').val());
+  //Define the hash algorithm
+  var hashalg = 'sha1';
+  var sign = rsa.sign(msg, hashalg);
+  //the value of sign is given in hex
+  return sign;
+}
+
+$('#create_rx').click(function(){
+  //Get Public key
+  var public_key = $('#pub_key').val();
+  //console.log(public_key);
+  ///// Encrytp data /////
+  var medic_name = $('#medic_name').val();
+  var medic_name_encrypt = encrypt_data(medic_name, public_key);
+  //console.log(medic_name);
+  var medic_hospital = "IMSS";
+  var medic_hospital_encrypt = encrypt_data(medic_hospital, public_key);
+  //console.log(medic_hospital);
+  var medic_cedula = "1234567890"
+  var medic_cedula_encrypt = encrypt_data(medic_cedula, public_key);
+  //console.log(medic_cedula);
+  var patient_name = $('#patient_name').val();
+  var patient_name_encrypt = encrypt_data(patient_name, public_key);
+  //console.log(patient_name);
+  var patiente_age = 26;
+  var patiente_age_encrypt = encrypt_data(patiente_age, public_key);
+  //console.log(patiente_age);
+  var diagnosis = "N/A";
+  var diagnosis_encrypt = encrypt_data(diagnosis, public_key);
+  //console.log(diagnosis);
+  var location = $('#location').val();
+  var location_encrypt = encrypt_data(location, public_key);
+  //console.log(location);
+
+  ///// Data not encrytp /////
+  var medications = [];
+  var time = "2018-06-06T07:46:12.576196";
+
+  ///// Create msg /////
+  var msg = public_key + medic_name + medic_hospital + medic_cedula + patient_name + patiente_age + diagnosis + location + medications + time;
+  //console.log(msg)
+
+  var sign = sign_msg(msg);
+  //console.log(sign);
+
+  var posting = $.post("http://127.0.0.1:8000/api/v1/rx-endpoint/",
+                  {
+                    patiente_age: patiente_age_encrypt,
+                    timestamp : time,
+                    public_key : "63636f70795f7265670a5f7265636f6e7374727563746f720a70310a28637273612e6b65790a5075626c69634b65790a70320a635f5f6275696c74696e5f5f0a6f626a6563740a70330a4e745270340a284c383935383035343431303037353737343836353330343639393734383236383039333138383038333336373233333639323835393330363133313033343932343630313734353633393334313838363433373634363134343231313039363331303030353234363338313630343032383131333236363031393430303533353831363035343533333636333737313132353536323534303937314c0a4936353533370a74622e",
+                    //"medications": [
+                    //                {"presentation": " Etacortilen ( Dexametasona 20 monodosis)", "instructions": "Med Lunes test"},
+                    //                {"presentation": "Med 2", "instructions": "Med Lunes test 2"}],
+                    patient_name: patient_name_encrypt,
+                    medic_hospital: medic_hospital_encrypt,
+                    location: location_encrypt,
+                    diagnosis: diagnosis_encrypt,
+                    signature: sign,
+                    medic_cedula: medic_cedula_encrypt,
+                    medic_name: medic_name_encrypt
+                  }, "json");
+
+  /*var posting = $.post("http://127.0.0.1:8000/api/v1/rx-endpoint/",
+                {
+                patient_age: 34,
+                timestamp: "2018-06-05T07:46:12.576196",
+                public_key: "63636f70795f7265670a5f7265636f6e7374727563746f720a70310a28637273612e6b65790a5075626c69634b65790a70320a635f5f6275696c74696e5f5f0a6f626a6563740a70330a4e745270340a284c383935383035343431303037353737343836353330343639393734383236383039333138383038333336373233333639323835393330363133313033343932343630313734353633393334313838363433373634363134343231313039363331303030353234363338313630343032383131333236363031393430303533353831363035343533333636333737313132353536323534303937314c0a4936353533370a74622e",
+                medications: [
+                    {presentation1: " Etacortilen ( Dexametasona 20 monodosis)", instructions1: "Med Lunes test"},
+                    {presentation2: "Med 2", instructions2: "Med Lunes test 2"}],
+                patient_name: "Xtornasol512",
+                medic_hospital: "N/A",
+                location: "N/A",
+                diagnosis: "Hola",
+                signature: "FiQBQIimp0YgnxYuI4kFrME9CTxu9T7eTGOmlG+Bs5alN+7MXtQRr4KgD0rANNf/hIbLZWt5A/FPLOWtv/UriA==",
+                medic_cedula: "5555521",
+                medic_name: "Everardo J. Barojas M."
+                }, "json");
+*/
+  posting.done(function(data, text_status){
+    console.log(data);
+    console.log(text_status);
+  });
+  posting.fail(function(xhr, textStatus, errorThrown){
+    //alert(xhr.responseText);
+    $("#result").html(tx_error);
+    console.log(xhr.responseText);
+  });
+  posting.always(function(){
+    console.log("Finish POST!");
+  });
 });
 
 // function mostrarContenido(contenido) {
@@ -304,3 +407,5 @@ $('#button_send_tx').click(function(){
 //  PRIVATE_KEY = value;
 //  });
 //}
+
+
