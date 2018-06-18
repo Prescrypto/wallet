d6e8ab169d8b85992d22e6f5206005d04906c01b
Miguel Evangelista <miguel.eva.alv@gmail.com>
2018-06-18 14:14:29 -0500

    add button create

--- js/genometrics.js
+++ js/genometrics.js
@@ -3,3 +3,10 @@ var add = document.getElementById ("addButton");
 function addFunc(){
         genocontent.innerHTML += `<a>${document.getElementById("genome").innerHTML}</a>`
 }
+
+function addForm(){
+        formcontent.innerHTML += `<ul>${document.getElementById("inputGroup").innerHTML}</ul>`
+}
+
+function removeForm(){
+}
