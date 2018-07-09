76e21552eb179cf325f77ba759f9d4087f9babaf
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-07-09 16:33:02 -0500

    Let use in development mode too!

--- js/wallet.js
+++ js/wallet.js
@@ -1,3 +1,11 @@
+(function (window){
+  function set_baseurl(baseurl){
+    // Set base url to Genobank Navigator, produdccion or developtment server
+    window.baseurl = (window.location.protocol === "https:") ? "https://genobank.herokuapp.com/" : baseurl;
+  };
+  set_baseurl("http://127.0.0.1:8000/");
+})(window);
+
 function save_key(private_key, public_key){
   //Save keys in localforage
   localforage.setItem('privatekey', private_key).then(function(value){
