6edfcbd5b67a3f928ac86cd5ca6f743385ae0cd8
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-07-09 13:29:13 -0500

    fix create keys button

--- js/wallet.js
+++ js/wallet.js
@@ -1,7 +1,13 @@
 function save_key(private_key, public_key){
   //Save keys in localforage
-  localforage.setItem('privatekey', private_key);
-  localforage.setItem('publickey', public_key);
-  console.log("Set keys on Localstorage");
+  localforage.setItem('privatekey', private_key).then(function(value){
+    // set privatekey
+    localforage.setItem('publickey', public_key).then(function(value){
+      console.log("Success Generated Keys!");
+      window.open("/createkeys.html","_self");
+    });
+  }).catch(function(e){
+    console.log(e);
+  });
 }
 
