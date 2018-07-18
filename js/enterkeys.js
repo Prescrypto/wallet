62e2d5c2ff9d707d0c82d2b40b6a2794950499f1
Miguel Evangelista <miguel.eva.alv@gmail.com>
2018-07-18 16:10:50 -0500

    Add function for read files of private key and public key

--- js/enterkeys.js
+++ js/enterkeys.js
@@ -1,9 +1,9 @@
-var bar = document.getElementById('js-progressbar');
+var bar_privakey = document.getElementById('load_privkey');
 
     UIkit.upload('.js-upload', {
 
         url: '',
-        multiple: true,
+        multiple: false,
 
         beforeSend: function () {
             console.log('beforeSend', arguments);
@@ -24,30 +24,87 @@ var bar = document.getElementById('js-progressbar');
         loadStart: function (e) {
             console.log('loadStart', arguments);
 
-            bar.removeAttribute('hidden');
-            bar.max = e.total;
-            bar.value = e.loaded;
+            bar_privakey.removeAttribute('hidden');
+            bar_privakey.max = e.total;
+            bar_privakey.value = e.loaded;
         },
 
         progress: function (e) {
             console.log('progress', arguments);
 
-            bar.max = e.total;
-            bar.value = e.loaded;
+            bar_privakey.max = e.total;
+            bar_privakey.value = e.loaded;
         },
 
         loadEnd: function (e) {
             console.log('loadEnd', arguments);
 
-            bar.max = e.total;
-            bar.value = e.loaded;
+            bar_privakey.max = e.total;
+            bar_privakey.value = e.loaded;
         },
 
         completeAll: function () {
             console.log('completeAll', arguments);
 
             setTimeout(function () {
-                bar.setAttribute('hidden', 'hidden');
+                bar_privakey.setAttribute('hidden', 'hidden');
+            }, 1000);
+
+            alert('Upload Completed');
+        }
+
+    });
+
+var bar_pubkey = document.getElementById('load_pubkey');
+
+    UIkit.upload('.js-upload', {
+
+        url: '',
+        multiple: false,
+
+        beforeSend: function () {
+            console.log('beforeSend', arguments);
+        },
+        beforeAll: function () {
+            console.log('beforeAll', arguments);
+        },
+        load: function () {
+            console.log('load', arguments);
+        },
+        error: function () {
+            console.log('error', arguments);
+        },
+        complete: function () {
+            console.log('complete', arguments);
+        },
+
+        loadStart: function (e) {
+            console.log('loadStart', arguments);
+
+            bar_pubkey.removeAttribute('hidden');
+            bar_pubkey.max = e.total;
+            bar_pubkey.value = e.loaded;
+        },
+
+        progress: function (e) {
+            console.log('progress', arguments);
+
+            bar_pubkey.max = e.total;
+            bar_pubkey.value = e.loaded;
+        },
+
+        loadEnd: function (e) {
+            console.log('loadEnd', arguments);
+
+            bar_pubkey.max = e.total;
+            bar_pubkey.value = e.loaded;
+        },
+
+        completeAll: function () {
+            console.log('completeAll', arguments);
+
+            setTimeout(function () {
+                bar_pubkey.setAttribute('hidden', 'hidden');
             }, 1000);
 
             alert('Upload Completed');
