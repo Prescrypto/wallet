1599e4568d82b52d6520ba9efd7f0e754831fe16
Miguel Evangelista <miguel.eva.alv@gmail.com>
2018-07-19 15:31:06 -0500

    change variable bar by bar_privkey, load by load pub key and remove variable bar_privkey

--- js/enterkeys.js
+++ js/enterkeys.js
@@ -1,11 +1,11 @@
-var bar_privakey = document.getElementById('load_privkey');
+var bar = document.getElementById('load');
 
     UIkit.upload('.js-upload', {
 
-        url: '',
+        url     : '',
         multiple: false,
 
-        beforeSend: function () {
+        beforeSend: function (environment) {
             console.log('beforeSend', arguments);
         },
         beforeAll: function () {
@@ -14,7 +14,7 @@ var bar_privakey = document.getElementById('load_privkey');
         load: function () {
             console.log('load', arguments);
         },
-        error: function () {
+        error: function (e) {
             console.log('error', arguments);
         },
         complete: function () {
@@ -24,87 +24,30 @@ var bar_privakey = document.getElementById('load_privkey');
         loadStart: function (e) {
             console.log('loadStart', arguments);
 
-            bar_privakey.removeAttribute('hidden');
-            bar_privakey.max = e.total;
-            bar_privakey.value = e.loaded;
+            bar.removeAttribute('hidden');
+            bar.max = e.total;
+            bar.value = e.loaded;
         },
 
         progress: function (e) {
             console.log('progress', arguments);
 
-            bar_privakey.max = e.total;
-            bar_privakey.value = e.loaded;
+            bar.max = e.total;
+            bar.value = e.loaded;
         },
 
         loadEnd: function (e) {
             console.log('loadEnd', arguments);
 
-            bar_privakey.max = e.total;
-            bar_privakey.value = e.loaded;
+            bar.max = e.total;
+            bar.value = e.loaded;
         },
 
         completeAll: function () {
             console.log('completeAll', arguments);
 
             setTimeout(function () {
-                bar_privakey.setAttribute('hidden', 'hidden');
-            }, 1000);
-
-            alert('Upload Completed');
-        }
-
-    });
-
-var bar_pubkey = document.getElementById('load_pubkey');
-
-    UIkit.upload('.js-upload', {
-
-        url: '',
-        multiple: false,
-
-        beforeSend: function () {
-            console.log('beforeSend', arguments);
-        },
-        beforeAll: function () {
-            console.log('beforeAll', arguments);
-        },
-        load: function () {
-            console.log('load', arguments);
-        },
-        error: function () {
-            console.log('error', arguments);
-        },
-        complete: function () {
-            console.log('complete', arguments);
-        },
-
-        loadStart: function (e) {
-            console.log('loadStart', arguments);
-
-            bar_pubkey.removeAttribute('hidden');
-            bar_pubkey.max = e.total;
-            bar_pubkey.value = e.loaded;
-        },
-
-        progress: function (e) {
-            console.log('progress', arguments);
-
-            bar_pubkey.max = e.total;
-            bar_pubkey.value = e.loaded;
-        },
-
-        loadEnd: function (e) {
-            console.log('loadEnd', arguments);
-
-            bar_pubkey.max = e.total;
-            bar_pubkey.value = e.loaded;
-        },
-
-        completeAll: function () {
-            console.log('completeAll', arguments);
-
-            setTimeout(function () {
-                bar_pubkey.setAttribute('hidden', 'hidden');
+                bar.setAttribute('hidden', 'hidden');
             }, 1000);
 
             alert('Upload Completed');
