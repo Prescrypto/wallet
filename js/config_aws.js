db861bac534acdac3732e39c6cbbf468125a952d
valerybriz <valerybriz@gmail.com>
2018-08-03 15:07:05 -0500

    lastest fixes for upload file

--- js/config_aws.js
+++ js/config_aws.js
@@ -1,8 +1,8 @@
 /*Configuration file for AWS */
-var AWS_AccessKeyId = 'AKIAJKIYL2H5QTQBIGWQ'; 
-var AWS_SecretAccessKey = '6SbsVW0jggYSyKXTnHImuQhu3HccjVIYfYvrM3AT'; 
+var AWS_AccessKeyId = ''; 
+var AWS_SecretAccessKey = ''; 
 var AWS_Region = 'us-west-2';  // Configure your region
-var AWS_BucketName = 'valerybriz';
+var AWS_BucketName = '';
 var AWS_MaxKeys = 500; //How many objects will retrive (include folders and items)
 var AWS_Prefix = ''; //Stating folder, by default start on root
 var AWS_SignedUrl_Expires = 900; //This is the default value for expires getSignedUrl
