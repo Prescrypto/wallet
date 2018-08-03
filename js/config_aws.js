30eb617c817f587573e4a1d6a886ba0bf48b5e31
valerybriz <valerybriz@gmail.com>
2018-08-03 14:41:40 -0500

    added changes to genometrics for file encrypt and upload

---
+++ js/config_aws.js
@@ -0,0 +1,9 @@
+/*Configuration file for AWS */
+var AWS_AccessKeyId = 'AKIAJKIYL2H5QTQBIGWQ'; 
+var AWS_SecretAccessKey = '6SbsVW0jggYSyKXTnHImuQhu3HccjVIYfYvrM3AT'; 
+var AWS_Region = 'us-west-2';  // Configure your region
+var AWS_BucketName = 'valerybriz';
+var AWS_MaxKeys = 500; //How many objects will retrive (include folders and items)
+var AWS_Prefix = ''; //Stating folder, by default start on root
+var AWS_SignedUrl_Expires = 900; //This is the default value for expires getSignedUrl
+var AWS_ACL = 'public-read'; //Permissions on the new object
