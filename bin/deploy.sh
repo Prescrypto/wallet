d7b0d4ab0df23380809f8e865f895d3be6f66bd7
valerybriz <valerybriz@gmail.com>
2018-09-06 13:51:06 -0500

    added new deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,26 +1,69 @@
 #!/user/bin/ bash
 
-#We recommend the user to use this code after doing push
-
 echo "Start"
-#Remove remote branch
-git push origin --delete gh-pages
 
-#Remove local branch
-git branch gh-pages -d
+# Define the variables Day and Hour
+Day=`date +"%d-%m-%Y"`
+Hour=`date +"%H:%M"`
 
-#Create local branch
-git checkout -b gh-pages
+folder="$Day-$Hour"
+replace ="_"
+# Get your branch name 
+branch_name="$(git branch | grep \* | cut -d ' ' -f2)" || branch_name="(unnamed branch)"
+new_branch_name="$(echo $branch_name | sed -e 's/\//_/g')"
+echo "you are now in: $branch_name, $new_branch_name"
 
-#Save the changes
+# Move all files to file temporal   
+cp -r ./ ./../$folder
+
+# Remove file .git
+rm -rf ./../$folder/.git
+rm -rf ./../$folder/deploy.sh
+
+#check if gh-pages exists in remote
+git fetch
+gh_pages_exists="$(git branch --list gh-pages)"
+echo $gh_pages_exists
+if ["$gh_pages_exists" = "" ]; then
+    #if it doesnt exists then create it
+    echo "gh-pages doesnt exists"
+    git checkout -b gh-pages
+else
+    echo "gh-pages branch already exists."
+    git checkout gh-pages 
+    
+fi
+
+#check if there is a folder with the name of the current branch if it does, remove it
+if [ -d "deploy/$new_branch_name" ]; then
+    echo "this directory already exists"
+    cd deploy
+    rm -rf $new_branch_name/*
+    cd ../
+else
+    echo "this directory doesnt exists"
+    # Create and Move to folder deploy   
+    mkdir deploy
+    cd deploy
+    mkdir $new_branch_name
+    cd ../
+fi
+
+#copy all the temp folder to the gh-pages folder
+cp -r ./../$folder/* deploy/$new_branch_name
+
+#Remove folder auxiliar
+rm -rf ./../$folder
+
+# Add all files
 git add --all
 
-#Commit
-git commit -m "deploy with bash"
+# Make commit
+git commit -m "Deploy Branch"
 
-#Create remote branch
-git push --set-upstream origin gh-pages
-echo "Finish"
+# Git push 
+git push || git push --set-upstream origin gh-pages
 
-#run this code
-#bash .deploy.sh
+#get back to the initial branch
+git checkout $branch_name
+echo "finish deployment"
