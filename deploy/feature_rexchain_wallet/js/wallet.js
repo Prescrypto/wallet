07f4aea1559a501b3c3947cc9771c5613f7b2578
valerybriz <valerybriz@gmail.com>
2018-10-19 12:49:28 -0500

    Deploy Branch

---
+++ deploy/feature_rexchain_wallet/js/wallet.js
@@ -0,0 +1,20 @@
+(function (window){
+  function set_baseurl(baseurl){
+    // Set base url to Genobank Navigator, produdccion or developtment server
+    window.baseurl = (window.location.protocol === "https:") ? "https://www.rexchain.io/" : baseurl;
+  };
+  set_baseurl("http://127.0.0.1:8000");
+})(window);
+
+function save_key(private_key, public_key){
+  //Save keys in localforage
+  localforage.setItem('privatekey', private_key).then(function(value){
+    // set privatekey
+    localforage.setItem('publickey', public_key).then(function(value){
+      console.log("Success Generated Keys!");
+    });
+  }).catch(function(e){
+    console.log(e);
+  });
+}
+
