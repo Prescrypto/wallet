28e6a8a6b47377b2cfb03f83affc6425c29f92b6
valerybriz <valerybriz@gmail.com>
2018-11-07 12:21:55 -0600

    Deploy Branch

--- deploy/feature_rexchain_wallet/js/wallet.js
+++ deploy/feature_rexchain_wallet/js/wallet.js
@@ -3,7 +3,7 @@
     // Set base url to Rexchain Navigator, produdccion or developtment server
     window.baseurl = (window.location.protocol === "https:") ? "https://www.rexchain.io/" : baseurl;
   };
-  set_baseurl("http://127.0.0.1:8000");
+  set_baseurl("https://www.rexchain.io/");
 })(window);
 
 function save_key(private_key, public_key){
