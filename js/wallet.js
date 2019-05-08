569687b22277d3f6667d0455bd26a7f0eee0f9c4
Miguel Evangelista <miguel.eva.alv@gmail.com>
2019-05-08 13:28:11 -0500

    Revert "change blockchain endpoint for the make testing"
    This reverts commit 420698d3366cfc600a18e6e28995e960c64e88b2.

--- js/wallet.js
+++ js/wallet.js
@@ -3,8 +3,7 @@
     // Set base url to Rexchain Navigator, produdccion or developtment server
     window.baseurl = (window.location.protocol === "https:") ? "https://www.rexchain.io/" : baseurl;
   };
-  //set_baseurl("https://www.rexchain.io/");
-  set_baseurl("https://rexchain-dev-pr-42.herokuapp.com/");
+  set_baseurl("https://www.rexchain.io/");
 })(window);
 
 function save_key(private_key, public_key){
